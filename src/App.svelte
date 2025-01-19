c1607bc0620bf40b4a91a105b3e265624397ef1f
chingifox <vandan04suthar@gmail.com>
2025-01-19 10:28:43 +0530

    feat: setup responsive heading

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,24 @@
+<script>
+  let pageHeader = "Chingifox"
+</script>
 
+<h1 class="header">{pageHeader}</h1>
+
+<style>
+
+  @import url('https://fonts.googleapis.com/css2?family=Syne+Mono&display=swap');
+
+  .header {
+    font-size: clamp(3rem, 6rem, 8vw); /* Dynamically resize */
+    font-family: "Syne Mono", monospace; 
+    font-style: italic;
+    font-weight: 400;
+    text-align: center; /* Center align the text */
+    white-space: nowrap; /* Prevent the text from wrapping */
+    overflow: hidden; /* Hide overflow if text exceeds container */
+    @REM text-overflow: ellipsis; /* Show ellipsis if text is truncated */
+    margin: 0; /* Remove default margins */
+    padding: 1rem; /* Add some spacing */
+  }
+
+</style>
