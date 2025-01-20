9b6052b9f20c81ea9f2ab4b30a030689c83cfc74
chingifox <vandan04suthar@gmail.com>
2025-01-20 18:57:43 +0530

    Implement component-based architecture

---
+++ src/components/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+  let pageHeader = "Chingifox.Talk";
+</script>
+
+<h1 class="header">{pageHeader}</h1>
+
+<style>
+    .header {
+        font-family: "Syne mono", monospace;
+        font-size: clamp(3rem, 6rem, 8vw);
+        font-style: italic;
+        font-weight: 400;
+        text-align: center; /* Center align the text */
+        white-space: nowrap; /* Prevent text from wrapping */
+        overflow: visible; /* Ensure text isn't cut off */
+        margin: 0;
+        padding: 1rem;
+        max-width: 100vw; /* Ensure the header fits within the viewport */
+        line-height: 1.2; /* Improve readability */
+    }
+
+    /* Responsive Adjustments (Mobile-first) */
+    @media (max-width: 768px) {
+        .header {
+            font-size: clamp(3rem, 10vw, 6rem); /* Smaller size for small devices */
+        }
+    }
+</style>
---
+++ src/components/ThemeToggle.svelte
@@ -0,0 +1,58 @@
+<script>
+  const initialTheme = localStorage.getItem('theme') || 'dark';
+  document.documentElement.setAttribute('data-theme', initialTheme);
+  let isDarkTheme = initialTheme === 'dark';
+
+  const checkboxInput = () => {
+    const theme = isDarkTheme ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  };
+</script>
+
+<input 
+  id="theme-toggle-checkbox" 
+  type="checkbox"
+  bind:checked={isDarkTheme}
+  on:change={checkboxInput}
+/>
+<input 
+  id="theme-toggle-checkbox" 
+  type="checkbox"
+  bind:checked={isDarkTheme}
+  on:change={checkboxInput}
+/>
+
+<style>
+    #theme-toggle-checkbox {
+        position: fixed;
+        z-index: 100;
+        width: 3.6rem;  
+        height: 3.6rem;  
+        -webkit-appearance: none;
+        appearance: none;
+        border: none;
+        cursor: pointer;
+        background-image: url('src/images/switchOn.png');
+        background-size: contain;
+        background-position: center;
+        background-repeat: no-repeat;
+        transition: background-image 0.2s ease;
+    }
+
+    #theme-toggle-checkbox {
+        bottom: 2rem;  /* Enough space for larger screens */
+        right: 1.3rem;  /* Enough space for larger screens */
+    }
+
+    @media (max-width: 768px) {  /* Apply styles for screens smaller than 768px (tablets and phones) */
+        #theme-toggle-checkbox {
+            bottom: 1rem;  /* Less space on smaller screens */
+            right: 0.3rem;   /* Less space on smaller screens */
+        }
+    }
+
+    #theme-toggle-checkbox:checked {
+        background-image: url('src/images/switchOff.png'); /* Image for checked state */
+    }
+</style>
