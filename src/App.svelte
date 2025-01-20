d16f5a7e3ed3202b399fed24d159727b0ce41a30
chingifox <vandan04suthar@gmail.com>
2025-01-20 21:33:29 +0530

    style: datepicker

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script>
   import Header from './components/Header.svelte';
   import LightSwitch from './components/ThemeToggle.svelte';
+  import DatePicker from './components/DatePicker.svelte';
 </script>
 
 <main>
   <Header />
   <LightSwitch />
+  <br>
+  <DatePicker />
 </main>
---
+++ src/components/DatePicker.svelte
@@ -0,0 +1,17 @@
+<script>
+    const date = '1 January, 2025';
+</script>
+
+<div class="datepicker-container">
+    <p id="datepicker">{date}</p> 
+</div> 
+
+<style>
+    .datepicker-container {
+        display: flex;
+        justify-content: flex-end;
+        position: relative;
+        font: inherit;
+        margin-right: 20vw;
+    }
+</style>
